fc36b829c12b3cb85b92c615036e6b1b87b17552
xxchan <xxchan@sjtu.edu.cn>
2020-01-07 16:51:22 +0800

    Command line parsing

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -1,4 +1,4 @@
-use clap::{App, AppSettings, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 use kvs::{KvStore, Result};
 use std::path::Path;
 use std::process;
@@ -6,10 +6,10 @@ use std::process;
 fn main() -> Result<()> {
     let mut kv = KvStore::open(Path::new("."))?;
 
-    let matches = App::new(env!("CARGO_PKG_NAME"))
+    let matches = App::new("kvs-client")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .about("A key-value store client")
         .setting(AppSettings::DisableHelpSubcommand)
         .subcommands(vec![
             SubCommand::with_name("set")
@@ -17,21 +17,49 @@ fn main() -> Result<()> {
                 .args_from_usage(
                     "<KEY> 'the key you want to set'
                     <VALUE> 'the value you want to set to the key'",
+                )
+                .arg(
+                    Arg::with_name("addr")
+                        .long("addr")
+                        .takes_value(true)
+                        .value_name("IP-PORT")
+                        .default_value("127.0.0.1:4000")
+                        .help("the server address"),
                 ),
             SubCommand::with_name("get")
                 .about("Get the string value of a given string key")
-                .arg_from_usage("<KEY> 'the key you want to look up'"),
+                .arg_from_usage("<KEY> 'the key you want to look up'")
+                .arg(
+                    Arg::with_name("addr")
+                        .long("addr")
+                        .takes_value(true)
+                        .value_name("IP-PORT")
+                        .default_value("127.0.0.1:4000")
+                        .help("the server address"),
+                ),
             SubCommand::with_name("rm")
                 .about("Remove a given key")
-                .arg_from_usage("<KEY> 'the key you want to remove'"),
+                .arg_from_usage("<KEY> 'the key you want to remove'")
+                .arg(
+                    Arg::with_name("addr")
+                        .long("addr")
+                        .takes_value(true)
+                        .value_name("IP-PORT")
+                        .default_value("127.0.0.1:4000")
+                        .help("the server address"),
+                ),
         ])
         .get_matches();
 
     match matches.subcommand() {
-        ("set", Some(matches)) => kv.set(
-            matches.value_of("KEY").unwrap().to_owned(),
-            matches.value_of("VALUE").unwrap().to_owned(),
-        )?,
+        ("set", Some(matches)) => {
+            let addr = matches.value_of("addr").expect("wtf");
+            println!("{}", addr);
+            kv.set(
+                matches.value_of("KEY").unwrap().to_owned(),
+                matches.value_of("VALUE").unwrap().to_owned(),
+            )?
+        }
         ("get", Some(matches)) => {
             let value = kv.get(matches.value_of("KEY").unwrap().to_owned())?;
             match value {
--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use clap::{App, AppSettings, Arg, SubCommand};
+use kvs::{KvStore, Result};
+use std::path::Path;
+use std::process;
+
+fn main() -> Result<()> {
+    let matches = App::new("kvs-server")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("A key-value store server")
+        .setting(AppSettings::DisableHelpSubcommand)
+        .arg(
+            Arg::with_name("addr")
+                .long("addr")
+                .takes_value(true)
+                .value_name("IP-PORT")
+                .default_value("127.0.0.1:4000")
+                .help("the server address"),
+        )
+        .arg(
+            Arg::with_name("engine")
+                .long("engine")
+                .takes_value(true)
+                .value_name("ENGINE-NAME")
+                .possible_values(&["kvs", "sled"])
+                .help("the server address"),
+        )
+        .get_matches();
+
+    unimplemented!();
+}
