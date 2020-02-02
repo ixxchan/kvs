bc457c1bac333411a01e42a51f57989b664daab3
xxchan <xxchan@sjtu.edu.cn>
2020-02-02 16:06:03 +0800

    add log in kvs-server

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -3,6 +3,11 @@ use kvs::{KvStore, Result};
 use std::path::Path;
 use std::process;
 
+#[macro_use]
+extern crate log;
+use log::LevelFilter;
+use simplelog::*;
+
 fn main() -> Result<()> {
     let matches = App::new("kvs-server")
         .version(env!("CARGO_PKG_VERSION"))
@@ -23,9 +28,17 @@ fn main() -> Result<()> {
                 .takes_value(true)
                 .value_name("ENGINE-NAME")
                 .possible_values(&["kvs", "sled"])
+                .default_value("kvs")
                 .help("the server address"),
         )
         .get_matches();
 
-    unimplemented!();
+    let addr = matches.value_of("addr").expect("wtf");
+    let engine = matches.value_of("engine").expect("wtf");
+
+    TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Stderr);
+    info!("kvs-server {}", env!("CARGO_PKG_VERSION"));
+    info!("Storage engine: {}", engine);
+    info!("Listening on {}", addr);
+    Ok(())
 }
