84e248a8ee04b154bc7ba63f5d28bcfcb20c9573
xxchan <xxchan@sjtu.edu.cn>
2020-02-23 23:50:20 +0800

    create client reader & writer at initialization

--- src/client.rs
+++ src/client.rs
@@ -6,22 +6,24 @@ use std::io::Write;
 use std::net::{TcpStream, ToSocketAddrs};
 
 pub struct KvsClient {
-    stream: TcpStream,
+    reader: TcpStream,
+    writer: TcpStream,
 }
 
 impl KvsClient {
     /// Connect to the address of a server
     pub fn connect<A: ToSocketAddrs>(addr: A) -> Result<Self> {
-        let stream = TcpStream::connect(addr)?;
-        Ok(KvsClient { stream })
+        let reader = TcpStream::connect(addr)?;
+        let writer = reader.try_clone()?;
+        Ok(KvsClient { reader, writer })
     }
 
     /// Set the value of a string key in the server
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let request = Request::Set { key, value };
-        serde_json::to_writer(self.stream.try_clone()?, &request)?;
-        self.stream.flush();
-        let response = serde_json::from_reader(self.stream.try_clone()?)?;
+        serde_json::to_writer(&mut self.writer, &request)?;
+        self.writer.flush();
+        let response = serde_json::from_reader(&mut self.reader)?;
         match response {
             Response::Ok(_) => Ok(()),
             Response::Err(msg) => Err(failure::err_msg(msg)),
@@ -31,9 +33,9 @@ impl KvsClient {
     /// Get the string value of a given string key from the server
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         let request = Request::Get { key };
-        serde_json::to_writer(self.stream.try_clone()?, &request)?;
-        self.stream.flush();
-        let response = serde_json::from_reader(self.stream.try_clone()?)?;
+        serde_json::to_writer(&mut self.writer, &request)?;
+        self.writer.flush();
+        let response = serde_json::from_reader(&mut self.reader)?;
         match response {
             Response::Ok(value) => Ok(value),
             Response::Err(msg) => Err(failure::err_msg(msg)),
@@ -43,9 +45,9 @@ impl KvsClient {
     /// Remove a given key in the server
     pub fn remove(&mut self, key: String) -> Result<()> {
         let request = Request::Rm { key };
-        serde_json::to_writer(self.stream.try_clone()?, &request)?;
-        self.stream.flush();
-        let response = serde_json::from_reader(self.stream.try_clone()?)?;
+        serde_json::to_writer(&mut self.writer, &request)?;
+        self.writer.flush();
+        let response = serde_json::from_reader(&mut self.reader)?;
         match response {
             Response::Ok(_) => Ok(()),
             Response::Err(msg) => Err(failure::err_msg(msg)),
