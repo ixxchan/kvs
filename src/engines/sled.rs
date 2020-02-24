ab38827b45336e5e21357cfd41ff74027880d3d6
xxchan <xxchan@sjtu.edu.cn>
2020-02-24 22:31:08 +0800

    implement sled engine

--- src/engines/sled.rs
+++ src/engines/sled.rs
@@ -1,17 +1,38 @@
 use super::KvsEngine;
 use crate::Result;
-pub struct SledKvsEngine {}
+use std::path::PathBuf;
+
+pub struct SledKvsEngine {
+    db: sled::Db,
+}
 
 impl KvsEngine for SledKvsEngine {
     fn set(&mut self, key: String, value: String) -> Result<()> {
-        unimplemented!()
+        self.db.insert(key, value.into_bytes())?;
+        Ok(())
     }
 
     fn get(&mut self, key: String) -> Result<Option<String>> {
-        unimplemented!()
+        Ok(self
+            .db
+            .get(key)?
+            .map(|buf| String::from_utf8(buf.to_vec()))
+            .transpose()?)
     }
 
     fn remove(&mut self, key: String) -> Result<()> {
-        unimplemented!()
+        if let None = self.db.remove(key)? {
+            Err(failure::err_msg("Key not found"))
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl SledKvsEngine {
+    pub fn open<P: AsRef<std::path::Path>>(path: P) -> Result<Self> {
+        Ok(SledKvsEngine {
+            db: sled::open(path)?,
+        })
     }
 }
