4134c01a855a8ea18fe6bfcbe88a45bfdc0e4633
xxchan <xxchan@sjtu.edu.cn>
2020-02-29 16:35:48 +0800

    Adding multithreading to KvServer

--- src/server.rs
+++ src/server.rs
@@ -3,17 +3,22 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 use crate::protocol::{Request, Response};
+use crate::thread_pool::*;
 use crate::{KvsEngine, Result};
 
 /// K-V store server.
 pub struct KvsServer<E: KvsEngine> {
     engine: E,
+    pool: NaiveThreadPool,
 }
 
 impl<E: KvsEngine> KvsServer<E> {
     /// Create a `KvsServer` with given store engine
     pub fn new(engine: E) -> Self {
-        KvsServer { engine }
+        KvsServer {
+            engine,
+            pool: NaiveThreadPool::new(6).unwrap(),
+        }
     }
 
     pub fn run<A: ToSocketAddrs>(&mut self, addr: A) -> Result<()> {
@@ -22,9 +27,12 @@ impl<E: KvsEngine> KvsServer<E> {
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    if let Err(e) = self.handle_client(stream) {
-                        error!("Error when serving client: {}", e);
-                    }
+                    let engine = self.engine.clone();
+                    self.pool.spawn(|| {
+                        if let Err(e) = KvsServer::handle_client(engine, stream) {
+                            error!("Error when serving client: {}", e);
+                        }
+                    })
                 }
                 Err(e) => error!("Connection failed: {}", e),
             }
@@ -32,7 +40,7 @@ impl<E: KvsEngine> KvsServer<E> {
         Ok(())
     }
 
-    fn handle_client(&mut self, stream: TcpStream) -> Result<()> {
+    fn handle_client(engine: E, stream: TcpStream) -> Result<()> {
         let mut writer = stream.try_clone()?;
         let peer_addr = stream.peer_addr()?;
         debug!("Connected to {}", peer_addr);
@@ -41,15 +49,15 @@ impl<E: KvsEngine> KvsServer<E> {
             let request = request?;
             debug!("Receive request from {}: {:?}", peer_addr, request);
             let response = match request {
-                Request::Get { key } => match self.engine.get(key) {
+                Request::Get { key } => match engine.get(key) {
                     Ok(value) => Response::Ok(value),
                     Err(e) => Response::Err(format!("{}", e)),
                 },
-                Request::Set { key, value } => match self.engine.set(key, value) {
+                Request::Set { key, value } => match engine.set(key, value) {
                     Ok(()) => Response::Ok(None),
                     Err(e) => Response::Err(format!("{}", e)),
                 },
-                Request::Rm { key } => match self.engine.remove(key) {
+                Request::Rm { key } => match engine.remove(key) {
                     Ok(()) => Response::Ok(None),
                     Err(e) => Response::Err(format!("{}", e)),
                 },
