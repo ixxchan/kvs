97c9987f030837e555c8a7a489efbeb37a3870bf
xxchan <xxchan@sjtu.edu.cn>
2020-03-02 17:22:01 +0800

    update and disable pool_bench (GIVE UP!)

--- benches/pool_bench.rs
+++ benches/pool_bench.rs
@@ -1,20 +1,22 @@
 #[macro_use]
 extern crate log;
 
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use crossbeam_utils::sync::WaitGroup;
 use env_logger::Env;
-use rand::prelude::*;
 use std::thread;
 use std::time::Duration;
 use tempfile::TempDir;
 
 use kvs::thread_pool::*;
-use kvs::{KvStore, KvsClient, KvsServer, Result, SledKvsEngine};
+use kvs::{KvStore, KvsClient, KvsServer, SledKvsEngine};
+
+// SIZE = 1000 is problematic ???
+const SIZE: u32 = 100;
+const INPUTS: &[u32; 6] = &[1, 2, 4, 6, 8, 12];
 
 pub fn write_queued_kvstore(c: &mut Criterion) {
-    let inputs = &[1, 2, 4, 6, 8, 12];
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::from_env(Env::default().default_filter_or("debug")).init();
 
     c.bench_function_over_inputs(
         "write_queued_kvstore",
@@ -29,10 +31,11 @@ pub fn write_queued_kvstore(c: &mut Criterion) {
             // wait for server
             thread::sleep(Duration::from_secs(1));
 
-            let client_pool = SharedQueueThreadPool::new(1000).unwrap();
+            let client_pool = SharedQueueThreadPool::new(SIZE).unwrap();
             b.iter(|| {
                 let wg = WaitGroup::new();
-                for i in 0..1000 {
+                for i in 0..SIZE {
+                    info!("client{}", i);
                     let wg = wg.clone();
                     client_pool.spawn(move || {
                         match KvsClient::connect("127.0.0.1:5005") {
@@ -56,13 +59,11 @@ pub fn write_queued_kvstore(c: &mut Criterion) {
             });
             info!("b.iter ends");
         },
-        inputs,
+        INPUTS,
     );
 }
 
 pub fn read_queued_kvstore(c: &mut Criterion) {
-    let inputs = &[1, 2, 4, 6, 8, 12];
-
     c.bench_function_over_inputs(
         "read_queued_kvstore",
         |b, &&threads| {
@@ -70,21 +71,21 @@ pub fn read_queued_kvstore(c: &mut Criterion) {
             let temp_dir = TempDir::new().unwrap();
             let engine = KvStore::open(temp_dir.path()).unwrap();
             let mut server = KvsServer::new(engine, pool);
-            thread::spawn(move || server.run("127.0.0.1:4007"));
+            thread::spawn(move || server.run("127.0.0.1:5006"));
             // wait for server
             thread::sleep(Duration::from_secs(1));
 
-            for i in 0..1000 {
-                let mut client = KvsClient::connect("127.0.0.1:4007").unwrap();
+            for i in 0..SIZE {
+                let mut client = KvsClient::connect("127.0.0.1:5006").unwrap();
                 assert!(client.set(format!("key{}", i), "value".to_owned()).is_ok());
             }
-            let client_pool = SharedQueueThreadPool::new(1000).unwrap();
+            let client_pool = SharedQueueThreadPool::new(SIZE).unwrap();
             b.iter(|| {
                 let wg = WaitGroup::new();
-                for i in 0..1000 {
+                for i in 0..SIZE {
                     let wg = wg.clone();
                     client_pool.spawn(move || {
-                        let mut client = KvsClient::connect("127.0.0.1:4007").unwrap();
+                        let mut client = KvsClient::connect("127.0.0.1:5006").unwrap();
                         assert_eq!(
                             client.get(format!("key{}", i)).unwrap(),
                             (Some("value".to_owned()))
@@ -95,17 +96,175 @@ pub fn read_queued_kvstore(c: &mut Criterion) {
                 wg.wait();
             });
         },
-        inputs,
+        INPUTS,
     );
 }
 
-pub fn write_rayon_kvstore(c: &mut Criterion) {}
+pub fn write_rayon_kvstore(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "write_rayon_kvstore",
+        |b, &&threads| {
+            let pool = RayonThreadPool::new(threads).unwrap();
+            let temp_dir = TempDir::new().unwrap();
+            let engine = KvStore::open(temp_dir.path()).unwrap();
 
-pub fn read_rayon_kvstore(c: &mut Criterion) {}
+            info!("wtf");
+            let mut server = KvsServer::new(engine, pool);
+            thread::spawn(move || server.run("127.0.0.1:5007"));
+            // wait for server
+            thread::sleep(Duration::from_secs(1));
 
-pub fn write_rayon_sled(c: &mut Criterion) {}
+            let client_pool = RayonThreadPool::new(SIZE).unwrap();
+            b.iter(|| {
+                let wg = WaitGroup::new();
+                for i in 0..SIZE {
+                    info!("client{}", i);
+                    let wg = wg.clone();
+                    client_pool.spawn(move || {
+                        match KvsClient::connect("127.0.0.1:5007") {
+                            Ok(mut client) => {
+                                if let Err(e) = client.set(format!("key{}", i), "value".to_owned())
+                                {
+                                    error!("2 {}", e);
+                                    panic!();
+                                }
+                            }
+                            Err(e) => {
+                                error!("1 {}", e);
+                                panic!();
+                            }
+                        }
+                        drop(wg);
+                    })
+                }
+                wg.wait();
+                info!("one iteration ends");
+            });
+            info!("b.iter ends");
+        },
+        INPUTS,
+    );
+}
 
-pub fn read_rayon_sled(c: &mut Criterion) {}
+pub fn read_rayon_kvstore(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "read_rayon_kvstore",
+        |b, &&threads| {
+            let pool = RayonThreadPool::new(threads).unwrap();
+            let temp_dir = TempDir::new().unwrap();
+            let engine = KvStore::open(temp_dir.path()).unwrap();
+            let mut server = KvsServer::new(engine, pool);
+            thread::spawn(move || server.run("127.0.0.1:5008"));
+            // wait for server
+            thread::sleep(Duration::from_secs(1));
+
+            for i in 0..SIZE {
+                let mut client = KvsClient::connect("127.0.0.1:5008").unwrap();
+                assert!(client.set(format!("key{}", i), "value".to_owned()).is_ok());
+            }
+            let client_pool = RayonThreadPool::new(SIZE).unwrap();
+            b.iter(|| {
+                let wg = WaitGroup::new();
+                for i in 0..SIZE {
+                    let wg = wg.clone();
+                    client_pool.spawn(move || {
+                        let mut client = KvsClient::connect("127.0.0.1:5008").unwrap();
+                        assert_eq!(
+                            client.get(format!("key{}", i)).unwrap(),
+                            (Some("value".to_owned()))
+                        );
+                        drop(wg);
+                    })
+                }
+                wg.wait();
+            });
+        },
+        INPUTS,
+    );
+}
+
+pub fn write_rayon_sled(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "write_rayon_sled",
+        |b, &&threads| {
+            let pool = RayonThreadPool::new(threads).unwrap();
+            let temp_dir = TempDir::new().unwrap();
+            let engine = SledKvsEngine::open(temp_dir.path()).unwrap();
+
+            info!("wtf");
+            let mut server = KvsServer::new(engine, pool);
+            thread::spawn(move || server.run("127.0.0.1:5009"));
+            // wait for server
+            thread::sleep(Duration::from_secs(1));
+
+            let client_pool = RayonThreadPool::new(SIZE).unwrap();
+            b.iter(|| {
+                let wg = WaitGroup::new();
+                for i in 0..SIZE {
+                    info!("client{}", i);
+                    let wg = wg.clone();
+                    client_pool.spawn(move || {
+                        match KvsClient::connect("127.0.0.1:5009") {
+                            Ok(mut client) => {
+                                if let Err(e) = client.set(format!("key{}", i), "value".to_owned())
+                                {
+                                    error!("2 {}", e);
+                                    panic!();
+                                }
+                            }
+                            Err(e) => {
+                                error!("1 {}", e);
+                                panic!();
+                            }
+                        }
+                        drop(wg);
+                    })
+                }
+                wg.wait();
+                info!("one iteration ends");
+            });
+            info!("b.iter ends");
+        },
+        INPUTS,
+    );
+}
+
+pub fn read_rayon_sled(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "read_queued_sled",
+        |b, &&threads| {
+            let pool = RayonThreadPool::new(threads).unwrap();
+            let temp_dir = TempDir::new().unwrap();
+            let engine = SledKvsEngine::open(temp_dir.path()).unwrap();
+            let mut server = KvsServer::new(engine, pool);
+            thread::spawn(move || server.run("127.0.0.1:5010"));
+            // wait for server
+            thread::sleep(Duration::from_secs(1));
+
+            for i in 0..SIZE {
+                let mut client = KvsClient::connect("127.0.0.1:5010").unwrap();
+                assert!(client.set(format!("key{}", i), "value".to_owned()).is_ok());
+            }
+            let client_pool = RayonThreadPool::new(SIZE).unwrap();
+            b.iter(|| {
+                let wg = WaitGroup::new();
+                for i in 0..SIZE {
+                    let wg = wg.clone();
+                    client_pool.spawn(move || {
+                        let mut client = KvsClient::connect("127.0.0.1:5010").unwrap();
+                        assert_eq!(
+                            client.get(format!("key{}", i)).unwrap(),
+                            (Some("value".to_owned()))
+                        );
+                        drop(wg);
+                    })
+                }
+                wg.wait();
+            });
+        },
+        INPUTS,
+    );
+}
 
 criterion::criterion_group!(benches, write_queued_kvstore, read_queued_kvstore);
 criterion_main!(benches);
